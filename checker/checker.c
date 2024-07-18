0368051603742ca962c951046be378e010f8c38b
dicarval <diogo.c.santos10@gmail.com>
2024-07-18 18:49:10 +0100

    Adding files

--- checker/checker.c
+++ checker/checker.c
@@ -6,12 +6,25 @@
 /*   By: dicarval <dicarval@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 10:36:40 by dicarval          #+#    #+#             */
-/*   Updated: 2024/07/17 15:21:57 by dicarval         ###   ########.fr       */
+/*   Updated: 2024/07/18 18:47:05 by dicarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
+int	ft_strcmp(char* instr, char * comparing)
+{
+	int i;
+
+	i = 0;
+	while (instr && comparing)
+	{
+		if (instr[i] != comparing[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
 void	error_instr(t_stack **a, t_stack **b)
 {
 	if (b)
@@ -24,8 +37,10 @@ void	error_instr(t_stack **a, t_stack **b)
 
 int	sort_check(t_stack **a, t_stack **b)
 {
-	if (*a == NULL || (*a)->next == NULL || *b != NULL)
+	if (*a == NULL || (*a)->next == NULL)
 		error_instr(a, b);
+	if (*b != NULL)
+		return (1);
 	while (*a)
 	{
 		if ((*a)->next == NULL)
@@ -34,35 +49,36 @@ int	sort_check(t_stack **a, t_stack **b)
 			return (1);
 		*a = (*a)->next;
 	}
+	return (0);
 }
+
 
 void	sort_instr(char *read_instr, t_stack **a, t_stack **b)
 {
-	if (read_instr = "pa\n")
+	if (ft_strcmp(read_instr, "pa\n"))
 		pa(a, b, false);
-	else if (read_instr = "pb\n")
+	else if (ft_strcmp(read_instr, "pb\n"))
 		pb(a, b, false);
-	else if (read_instr = "sa\n")
+	else if (ft_strcmp(read_instr, "sa\n"))
 		sa(a, false);
-	else if (read_instr = "sb\n")
+	else if (ft_strcmp(read_instr, "sb\n"))
 		sb(a, false);
-	else if (read_instr = "ss\n")
+	else if (ft_strcmp(read_instr, "ss\n"))
 		ss(a, b, false);
-	else if (read_instr = "ra\n")
+	else if (ft_strcmp(read_instr, "ra\n"))
 		ra(a, false);
-	else if (read_instr = "rb\n")
+	else if (ft_strcmp(read_instr, "rb\n"))
 		rb(b, false);
-	else if (read_instr = "rr\n")
+	else if (ft_strcmp(read_instr, "rr\n"))
 		rr(a, b, false);
-	else if (read_instr = "rra\n")
+	else if (ft_strcmp(read_instr, "rra\n"))
 		rra(a, false);
-	else if (read_instr = "rrb\n")
+	else if (ft_strcmp(read_instr, "rrb\n"))
 		rrb(b, false);
-	else if (read_instr = "rrr\n")
+	else if (ft_strcmp(read_instr, "rrr\n"))
 		rrr(a, b, false);
 	else
 		error_instr(a, b);
-	free(read_instr);
 }
 
 int	main(int argc, char **argv)
@@ -76,9 +92,11 @@ int	main(int argc, char **argv)
 		return (0);
 	stack_creation(&a, argv);
 	fd = 0;
-	while ((read_instr = get_next_line(fd)))
+	read_instr = get_next_line(fd);
+	while (read_instr)
 	{
 		sort_instr(read_instr, &a, &b);
+		read_instr = get_next_line(fd);
 	}
 	if (sort_check(&a, &b))
 		write(1, "KO\n", 3);
--- checker/checker.h
+++ checker/checker.h
@@ -6,17 +6,18 @@
 /*   By: dicarval <dicarval@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 10:31:14 by dicarval          #+#    #+#             */
-/*   Updated: 2024/07/17 12:01:54 by dicarval         ###   ########.fr       */
+/*   Updated: 2024/07/18 18:37:33 by dicarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHECKER_H
 # define CHECKER_H
-# define BUFFER_SIZE 5
+
+# define BUFFER_SIZE 4
+
 # include "../push_swap/header/push_swap.h"
 # include "gnl/get_next_line.h"
-
-
-
+# include <stdio.h>
+# include <stdlib.h>
 
 # endif
